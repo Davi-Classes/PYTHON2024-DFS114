66c5fb43fb030a9ac41192747179a00f35c50a9c
Davi Gomes Lucciola <69369398+davi-lucciola@users.noreply.github.com>
2024-09-01 14:40:31 -0300

    Add files via upload

---
+++ Lógica de Programação/Aula 01/ex02.py
@@ -0,0 +1,6 @@
+dia = input('Digite o dia de seu nascimento: ')
+mes = input('Digite o mês de seu nascimento: ')
+ano = input('Digite o ano de seu nascimento: ')
+
+print(f'Sua data de nascimento é {dia}/{mes}/{ano}')
+print('Sua data de nascimento é ' + dia + '/' + mes + '/' + ano)
